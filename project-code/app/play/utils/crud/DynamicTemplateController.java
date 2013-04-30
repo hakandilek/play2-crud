2347ad28ec1503202a851cee9cfda6c7914e1a71
Hakan Dilek <hakandilek@gmail.com>
2013-04-30 12:15:19 +0200

    Verbose message for a missing template signature

--- project-code/app/play/utils/crud/DynamicTemplateController.java
+++ project-code/app/play/utils/crud/DynamicTemplateController.java
@@ -12,14 +12,13 @@ import play.mvc.Result;
 
 public class DynamicTemplateController extends Controller {
 
-
 	protected final ALogger log = Logger.of(getClass());
 
 	protected Result ok(String template, Parameters params) {
 		Content content;
 		try {
 			content = call("views.html." + template, "render", params);
-		} catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException | MethodNotFoundException e) {
 			return internalServerError(templateNotFound(template, params));
 		}
 		return ok(content);
@@ -29,19 +28,20 @@ public class DynamicTemplateController extends Controller {
 		Content content;
 		try {
 			content = call("views.html." + template, "render", params);
-		} catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException | MethodNotFoundException e) {
 			return internalServerError(templateNotFound(template, params));
 		}
 		return badRequest(content);
 	}
-	
+
 	private String templateNotFound(String template, Parameters params) {
 		StringBuilder sb = new StringBuilder("Template ");
 		sb.append(template).append("(");
 		Class<?>[] types = params.types();
 		for (int i = 0; i < types.length; i++) {
 			Class<?> type = types[i];
-			if (i != 0) sb.append(", ");
+			if (i != 0)
+				sb.append(", ");
 			sb.append(type.getSimpleName());
 		}
 		sb.append(") is not found");
@@ -51,13 +51,14 @@ public class DynamicTemplateController extends Controller {
 	protected <T> Parameters with() {
 		return new Parameters();
 	}
-	
+
 	protected <T> Parameters with(Class<?> paramType, T paramValue) {
 		return new Parameters(paramType, paramValue);
 	}
 
 	@SuppressWarnings("unchecked")
-	private <R> R call(String className, String methodName, Parameters params) throws ClassNotFoundException {
+	private <R> R call(String className, String methodName, Parameters params)
+			throws ClassNotFoundException, MethodNotFoundException {
 		ClassLoader classLoader = getClass().getClassLoader();
 		Object result = null;
 		Class<?> clazz = classLoader.loadClass(className);
@@ -65,8 +66,39 @@ public class DynamicTemplateController extends Controller {
 		Object[] paramValues = params.values();
 		Method method = ReflectionUtils.findMethod(clazz, methodName,
 				paramTypes);
+		if (method == null) {
+			throw new MethodNotFoundException(className, methodName, paramTypes);
+		}
 		result = ReflectionUtils.invokeMethod(method, null, paramValues);
 		return (R) result;
 	}
 
+	class MethodNotFoundException extends Exception {
+
+		private static final long serialVersionUID = 1L;
+		private String className;
+		private String methodName;
+		private Class<?>[] paramTypes;
+
+		public MethodNotFoundException(String className, String methodName,
+				Class<?>[] paramTypes) {
+			this.className = className;
+			this.methodName = methodName;
+			this.paramTypes = paramTypes;
+		}
+
+		public String toString() {
+			StringBuffer sb = new StringBuffer(className);
+			sb.append(".").append(methodName).append("(");
+			for (int i = 0; i < paramTypes.length; i++) {
+				Class<?> t = paramTypes[i];
+				sb.append(t.getSimpleName());
+				if (i < paramTypes.length - 1)
+					sb.append(", ");
+			}
+			sb.append(")");
+			return sb.toString();
+		}
+
+	}
 }
