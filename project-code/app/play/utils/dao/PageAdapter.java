7a79b5eba6c0636b17e5bfa0a9ebf4395bae19f2
Hakan Dilek <hakandilek@gmail.com>
2013-07-08 17:36:08 +0200

    introduced PageAdapter. closes #13

---
+++ project-code/app/play/utils/dao/PageAdapter.java
@@ -0,0 +1,62 @@
+package play.utils.dao;
+
+import com.avaje.ebean.Page;
+
+public abstract class PageAdapter<S, T> implements Page<T> {
+
+	protected Page<S> delegate;
+
+	public PageAdapter(Page<S> delegate) {
+		this.delegate = delegate;
+	}
+
+	/**
+	 * created a new page adapter. used to create prev/next pages
+	 * @param delegate page delegate
+	 * @return a new page adapter
+	 */
+	public abstract PageAdapter<S, T> create(Page<S> delegate);
+	
+	@Override
+	public int getTotalRowCount() {
+		return delegate.getTotalRowCount();
+	}
+
+	@Override
+	public int getTotalPageCount() {
+		return delegate.getTotalPageCount();
+	}
+
+	@Override
+	public int getPageIndex() {
+		return delegate.getPageIndex();
+	}
+
+	@Override
+	public boolean hasNext() {
+		return delegate.hasNext();
+	}
+
+	@Override
+	public boolean hasPrev() {
+		return delegate.hasPrev();
+	}
+
+	@Override
+	public Page<T> next() {
+		Page<S> p = delegate.next();
+		return create(p);
+	}
+
+	@Override
+	public Page<T> prev() {
+		Page<S> p = delegate.prev();
+		return create(p);
+	}
+
+	@Override
+	public String getDisplayXtoYofZ(String to, String of) {
+		return delegate.getDisplayXtoYofZ(to, of);
+	}
+
+}
