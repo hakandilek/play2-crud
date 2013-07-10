b79c3f4749dc1ed5d5cd8b6b3dc1a3664c0cda6a
Hakan Dilek <hakandilek@gmail.com>
2013-07-10 14:17:33 +0200

    remove play2-cache dependency

--- project-code/project/Build.scala
+++ project-code/project/Build.scala
@@ -11,17 +11,6 @@ object ApplicationBuild extends Build {
         javaCore, javaJdbc, javaEbean
     )
 
-    lazy val play2cache = play.Project("play2-cache", appVersion, appDependencies, path = file("modules/play2-cache/project-code")).settings(
-        publishArtifact in(Compile, packageDoc) := false,
-        publishMavenStyle := true,
-        publishTo <<= version { (v: String) =>
-        	if (v.trim.endsWith("SNAPSHOT"))
-    			Some(Resolver.file("file",  new File( "../../maven-repo/snapshots" )) )
-			else
-    			Some(Resolver.file("file",  new File( "../../maven-repo/releases" )) )
-        }
-    )
-
     val main = play.Project(appName, appVersion, appDependencies).settings(
         publishArtifact in(Compile, packageDoc) := false,
         publishMavenStyle := true,
@@ -31,6 +20,6 @@ object ApplicationBuild extends Build {
 			else
     			Some(Resolver.file("file",  new File( "../../maven-repo/releases" )) )
         }
-    ).dependsOn(play2cache).aggregate(play2cache)
+    )
 
 }
