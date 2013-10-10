3d1ccc3f3e7613196f089e587bbe2bdf707fd8a4
Hakan Dilek <hakandilek@gmail.com>
2013-10-10 17:16:18 +0200

    cleanup simple sample

--- samples/play2-crud-simple/conf/evolutions/default/1.sql
+++ samples/play2-crud-simple/conf/evolutions/default/1.sql
@@ -3,11 +3,24 @@
 
 # --- !Ups
 
+create table complex (
+  key                       bigint not null,
+  string_field              varchar(255),
+  long_string_field         varchar(256),
+  integer_field             integer,
+  double_field              double,
+  boolean_field             boolean,
+  date_field                timestamp,
+  constraint pk_complex primary key (key))
+;
+
 create table simple (
   key                       bigint not null,
   name                      varchar(255),
   constraint pk_simple primary key (key))
 ;
+
+create sequence complex_seq;
 
 create sequence simple_seq;
 
@@ -18,9 +31,13 @@ create sequence simple_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
+drop table if exists complex;
+
 drop table if exists simple;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists complex_seq;
 
 drop sequence if exists simple_seq;
 
