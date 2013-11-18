e413fd6df81d6e5e0d13f20ef8c94624ec60a02a
Hakan Dilek <hakandilek@gmail.com>
2013-11-18 12:33:24 +0100

    Fix Asset route lookup in templates. fixes #27 and #28.

--- project-code/app/play/utils/crud/Assets.java
+++ project-code/app/play/utils/crud/Assets.java
@@ -3,9 +3,10 @@ package play.utils.crud;
 import play.api.mvc.Action;
 import play.api.mvc.AnyContent;
 
-public class CrudAssets {
+public class Assets {
 
 	public static Action<AnyContent> at(String path, String file) {
 		return controllers.Assets.at(path, file);
 	}
+	
 }
