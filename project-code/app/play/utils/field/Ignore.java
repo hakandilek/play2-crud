9a7bf45c3ebc5a03112563957da7cc2e5e63a9a4
Ludovic Chane Won In <ludochane@gmail.com>
2014-11-12 22:30:32 +0100

    Adding an Ignore annotation to ignore fields in the MetaModel. When a field is annotated with @Ignore, it won't appear in the create/edit form.

---
+++ project-code/app/play/utils/field/Ignore.java
@@ -0,0 +1,21 @@
+package play.utils.field;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Indicates to ignore this field in the list of fields in the metamodel. <br>
+ * So it won't appear in the edit form.
+ * 
+ * @author Ludovic Chane Won In
+ */
+@Documented
+@Target(FIELD)
+@Retention(RUNTIME)
+public @interface Ignore {
+
+}
