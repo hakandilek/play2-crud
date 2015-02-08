8e3c77730370ccf498860305bacc1c7607d9b719
Ali Soleimani <alisol911@gmail.com>
2015-02-08 17:18:28 +0330

    updated dependencies version

--- project-code/project/Build.scala
+++ project-code/project/Build.scala
@@ -7,14 +7,14 @@ object ApplicationBuild extends Build {
 
     val appName         = "play2-crud"
     val appVersion      = "0.7.6-SNAPSHOT"
-    val appScalaVersion = "2.11.1"
+    val appScalaVersion = "2.11.4"
 
     val appDependencies = Seq(
         javaCore, javaJdbc, javaEbean, cache,
-        "org.webjars" % "bootstrap" % "3.3.1",
-        "org.webjars" % "font-awesome" % "4.2.0",
-        "org.webjars" % "json2" % "20110223",
-        "com.google.inject" % "guice" % "4.0-beta4"
+        "org.webjars" % "bootstrap" % "3.3.2",
+        "org.webjars" % "font-awesome" % "4.3.0-1",
+        "org.webjars" % "json2" % "20140204",
+        "com.google.inject" % "guice" % "4.0-beta5"
     )
 
     val root = Project(appName, file(".")).enablePlugins(play.PlayScala).settings(
